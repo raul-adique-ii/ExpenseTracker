90ac36c3c830e23808945621d32d99f60e43c237
raul-adique-ii <yaul2322@gmail.com>
2020-11-14 08:38:23 +0800

    Initialized package.json for server

---
+++ client/src/components/Balance.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography';
+
+import { useRecoilValue } from 'recoil'
+import { transactionState } from '../recoil/GlobalState'
+
+const useStyles = makeStyles((theme) => ({
+    balance: {
+        color: '#1b5e20',
+        // marginBottom: 30,
+        alignSelf: 'center'
+    },
+    container: {
+        padding: '10px',
+        // paddingBottom: '5px',
+        marginBottom: '20px',
+        alignSelf: 'center',
+        display: 'flex',
+        justifyContent: 'space-around',
+        flexDirection: 'column'
+    },
+    title: {
+        color: '#2F0C49',
+        alignSelf: 'center'
+    }
+}))
+
+const Balance = () => {
+    const classes = useStyles()
+    const { total } = useRecoilValue(transactionState)
+
+    return (
+        <>
+        <Paper className={classes.container} elevation={6}>
+          <Typography className={classes.title} variant='h6'>Your Balance</Typography>
+          <Typography className={classes.balance} variant='h4'>${total}</Typography>
+        </Paper>
+        </>
+    )
+}
+
+export default Balance
---
+++ client/src/components/TransactionForm.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button'
+import { purple } from '@material-ui/core/colors';
+import Divider from '@material-ui/core/Divider'
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography'
+import { v4 as uuidv4 } from 'uuid'
+
+import { useSetRecoilState } from 'recoil';
+import { initialState } from '../recoil/GlobalState';
+
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        margin: theme.spacing(2),
+    },
+    formContainer: {
+        padding: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    headerContainer: {
+        padding: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    title: {
+        color: '#fff',
+        alignSelf: 'center'
+    },
+ 
+}))
+const theme = createMuiTheme({
+    palette: {
+      primary: purple,
+    },
+  });
+const TransactionForm = () => {
+    const classes = useStyles()
+    const [text, setText] = useState('')
+    const [amount, setAmount] = useState('')
+    const setTransactionList = useSetRecoilState(initialState)
+
+    const addItem = () => {
+        setTransactionList((oldList) => [
+            ...oldList,
+            {
+                id: uuidv4(),
+                text: text.toUpperCase(),
+                amount: +amount
+            }
+        ])
+        setText('')
+        setAmount('')
+    }
+
+    return (
+        <>
+            <div className={classes.headerContainer}>
+                <Typography className={classes.title} variant='h4'>Add New Transaction</Typography>
+                <Divider />
+            </div>
+            <div>
+                <Paper className={classes.formContainer} elevation={6}>
+                    <TextField label='Enter Transaction' fullWidth value={text} onChange={(e) => setText(e.target.value)} />
+                    <TextField label='Enter Amount' fullWidth value={amount} onChange={(e) => setAmount(e.target.value)}  />
+                    <Typography variant='h6'>(- for expense, + for income)</Typography>
+                    <ThemeProvider theme={theme}>
+                        <Button onClick={addItem} className={classes.button} variant='contained' color='primary'>
+                            Add Transaction
+                        </Button>
+                    </ThemeProvider>
+                </Paper>
+            </div>
+        </>
+    )
+}
+
+export default TransactionForm
